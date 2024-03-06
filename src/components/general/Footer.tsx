4efd8864a759e32d3fb1f64ae894f8a1f8943fe6
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2024-03-06 12:23:58 -0300

    Correção do footer
    Alteração no footer para que ele só apareça em telas menores que 992px e adição de alt nas imagens.

--- src/components/general/Footer.tsx
+++ src/components/general/Footer.tsx
@@ -5,20 +5,27 @@ import arrowLeftIcon from "../../assets/images/caretLeftSolid.svg";
 
 export const Footer = () => {
   const navigate = useNavigate();
+  const mobile = window.innerWidth <= 992;
 
   return (
-    <nav className="container-footer">
-      <ul className="navigation-footer">
-        <li>
-          <img src={arrowLeftIcon} onClick={() => navigate(-1)} />
-        </li>
-        <li>
-          <img src={homeIcon} onClick={() => navigate("/home")}/>
-        </li>
-        <li>
-          <img src={userIcon} onClick={() => navigate("/profile")}/>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {mobile ? (
+        <nav className="container-footer">
+          <ul className="navigation-footer">
+            <li>
+              <img src={arrowLeftIcon} onClick={() => navigate(-1)} alt="Voltar"/>
+            </li>
+            <li>
+              <img src={homeIcon} onClick={() => navigate("/home")} alt="Home"/>
+            </li>
+            <li>
+              <img src={userIcon} onClick={() => navigate("/profile")} alt="Perfil do usuário"/>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
