56248ca908b3a753650261aa535509a5e724a2ae
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2024-03-26 11:38:15 -0300

    Novas alterções no estilos
    Mudança no logo, no footer, header, login e no menuOptions e no componente do menuOptions

--- src/components/general/MenuOptions.tsx
+++ src/components/general/MenuOptions.tsx
@@ -1,83 +1,29 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import userCardIcon from '../../assets/images/userCardSolid.svg';
 import userCardIconWhite from '../../assets/images/userCardSolidWhite.svg';
-import userPlusIcon from '../../assets/images/userPlusSolid.svg';
 import userPlusIconWhite from '../../assets/images/userPlusSolidWhite.svg';
-import clientsIcon from '../../assets/images/clientsSolid.svg';
 import clientsIconWhite from '../../assets/images/clientsSolidWhite.svg';
-import glassIcon from '../../assets/images/glassSolid.svg';
 import glassIconWhite from '../../assets/images/glassSolidWhite.svg';
 
 export const MenuOptions = () => {
   const navigate = useNavigate();
 
-  const [isHovered, setIsHovered] = useState({
-    profile: false,
-    userRegister: false,
-    clientRegister: false,
-    clientSearch: false,
-  });
-
-  const handleMouseEnter = (key: any) => {
-    setIsHovered((prevState) => ({
-      ...prevState,
-      [key]: true,
-    }));
-  };
-
-  const handleMouseLeave = (key: any) => {
-    setIsHovered((prevState) => ({
-      ...prevState,
-      [key]: false,
-    }));
-  };
-
   return (
     <div className="container-options">
       <div className="container-menu">
-        <div
-          onClick={() => navigate('/profile')}
-          onMouseEnter={() => handleMouseEnter('profile')}
-          onMouseLeave={() => handleMouseLeave('profile')}
-        >
-          <img
-            src={isHovered.profile ? userCardIconWhite : userCardIcon}
-            alt="Perfil do usuário"
-          />
+        <div onClick={() => navigate('/profile')}>
+          <img src={userCardIconWhite} alt="Perfil do usuário" />
           <p>Perfil</p>
         </div>
-        <div
-          onClick={() => navigate('/user_register')}
-          onMouseEnter={() => handleMouseEnter('userRegister')}
-          onMouseLeave={() => handleMouseLeave('userRegister')}
-        >
-          <img
-            src={isHovered.userRegister ? userPlusIconWhite : userPlusIcon}
-            alt="Cadastro de usuários"
-          />
+        <div onClick={() => navigate('/user_register')}>
+          <img src={userPlusIconWhite} alt="Cadastro de usuários" />
           <p>Cadastro de usuários</p>
         </div>
-        <div
-          onClick={() => navigate('/client_register')}
-          onMouseEnter={() => handleMouseEnter('clientRegister')}
-          onMouseLeave={() => handleMouseLeave('clientRegister')}
-        >
-          <img
-            src={isHovered.clientRegister ? clientsIconWhite : clientsIcon}
-            alt="Cadastro de clientes."
-          />
+        <div onClick={() => navigate('/client_register')}>
+          <img src={clientsIconWhite} alt="Cadastro de clientes." />
           <p>Cadastro de clientes</p>
         </div>
-        <div
-          onClick={() => navigate('/client_search')}
-          onMouseEnter={() => handleMouseEnter('clientSearch')}
-          onMouseLeave={() => handleMouseLeave('clientSearch')}
-        >
-          <img
-            src={isHovered.clientSearch ? glassIconWhite : glassIcon}
-            alt="Pesquisa de clientes."
-          />
+        <div onClick={() => navigate('/client_search')}>
+          <img src={glassIconWhite} alt="Pesquisa de clientes." />
           <p>Pesquisa de clientes</p>
         </div>
       </div>
