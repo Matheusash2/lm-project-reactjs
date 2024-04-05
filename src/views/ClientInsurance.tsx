760c17a131dcf4ee1df6ee978e13b2fefc21d501
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2024-04-04 23:22:50 -0300

    feat(view): Adicionar view do ClientInsurance
    Nova view ClientInsurance para função de requerimento do seguro desemprego.

---
+++ src/views/ClientInsurance.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '../components/general/Layout';
+
+export const ClientInsurance = () => {
+  return (
+    <Layout>
+      <div>Seguro desemprego</div>
+    </Layout>
+  );
+};
