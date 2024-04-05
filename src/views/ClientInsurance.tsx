453d6d0dc88c530f0ee7ae26170902c54828e7db
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2024-04-04 23:45:04 -0300

    feat(view): Adicionar iframe na view ClientInsurance

--- src/views/ClientInsurance.tsx
+++ src/views/ClientInsurance.tsx
@@ -3,7 +3,13 @@ import { Layout } from '../components/general/Layout';
 export const ClientInsurance = () => {
   return (
     <Layout>
-      <div>Seguro desemprego</div>
+      <div>
+        <h2>Seguro desemprego</h2>
+        <iframe
+          src="https://contatos.trabalho.gov.br/"
+          title="Site do formulário para liberação do seguro desemprego."
+        ></iframe>
+      </div>
     </Layout>
   );
 };
